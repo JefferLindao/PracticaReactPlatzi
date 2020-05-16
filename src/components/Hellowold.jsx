dba4c8aeb2420f993b524ef9dfbf6c4e9ce1f9f8
JefferLindao <jefferlindao@gmail.com>
2020-05-16 18:59:40 -0500

    Bienvenido a React

---
+++ src/components/Hellowold.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import '../assets/style/App.scss';
+
+const HelloWorld = () => (
+  <h1>Hola Mundo</h1>
+);
+export default HelloWorld;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import HelloWorld from './components/Hellowold';
+
+ReactDOM.render( < HelloWorld / > , document.getElementById('app'));
