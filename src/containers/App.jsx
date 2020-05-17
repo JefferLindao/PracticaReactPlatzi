cc6cfdf4918f9d5cfe92a8284ec985f7b0b1e1d9
JefferLindao <jefferlindao@gmail.com>
2020-05-17 17:37:57 -0500

    Estuctura y estilo del Footer

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -4,6 +4,7 @@ import Search from '../components/Search';
 import Categories from '../components/Categories';
 import Carousel from '../components/Carousel';
 import CarouselItem from '../components/CarouselItem';
+import Footer from '../components/Footer';
 import '../assets/style/App.scss';
 
 const App = () => (
@@ -16,11 +17,9 @@ const App = () => (
         <CarouselItem />
         <CarouselItem />
         <CarouselItem />
-        <CarouselItem />
-        <CarouselItem />
       </Carousel>
     </Categories>
-
+    <Footer />
   </div>
 );
 export default App;
