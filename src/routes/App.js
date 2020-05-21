3f34c842bf4df6bf87124210e02b0c5e911652fb
JefferLindao <jefferlindao@gmail.com>
2020-05-20 23:38:21 -0500

    Container Login

--- src/routes/App.js
+++ src/routes/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Home from '../containers/Home';
+import Login from '../containers/Login';
 
 const App = () => (
   <BrowserRouter>
     <Route exact path="/" component={Home} />
+    <Login exact pacth="/Login" component={Login} />
   </BrowserRouter>
 );
 
