7e8b1009ec38d6d3a32e93092ec61173e8f82f35
JefferLindao <jefferlindao@gmail.com>
2020-05-21 16:48:58 -0500

    Not Found

--- src/routes/App.js
+++ src/routes/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Router } from 'react-router-dom';
 import Home from '../containers/Home';
 import Login from '../containers/Login';
 import Register from '../containers/Register';
+import NotFound from '../containers/NotFound';
 
 const App = () => (
   <BrowserRouter>
@@ -10,6 +11,7 @@ const App = () => (
       <Route exact path="/" component={Home} />
       <Route exact path="/login" component={Login} />
       <Route exact path="/register" component={Register} />
+      <Route component={NotFound} />
     </Switch>
   </BrowserRouter>
 );
