88a9720b48044ea021140cc097c7f252bee6b251
JefferLindao <jefferlindao@gmail.com>
2020-05-24 18:57:31 -0500

    Formulario de login con Redux

--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -1,11 +1,13 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 import React, { useState } from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { loginRequest } from '../actions';
 import googleIcon from '../assets/static/google-icon.png';
 import twitterIcon from '../assets/static/twitter-icon.png';
 import '../assets/style/components/Login.scss';
 
-const Login = () => {
+const Login = (props) => {
   const [form, setValues] = useState({
     email: ''
   });
@@ -18,7 +20,8 @@ const Login = () => {
   };
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log(form);
+    props.loginRequest(form);
+    props.history.push('/');
   };
   return (
     <section className="login">
@@ -70,4 +73,7 @@ const Login = () => {
   );
 };
 
-export default Login;
+const mapDispatchToProps = {
+  loginRequest
+};
+export default connect(null, mapDispatchToProps)(Login);
