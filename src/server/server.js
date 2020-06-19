240ecfe844f71b78ff31179cbefb6e5ba3e3a76c
JefferLindao <jefferlindao@gmail.com>
2020-06-18 20:14:47 -0500

    Realizar el renderizado desde el servidor

--- src/server/server.js
+++ src/server/server.js
@@ -1,6 +1,15 @@
 import express from 'express'
 import dotnev from 'dotenv'
 import webpack from 'webpack'
+import React from 'react';
+import { renderToString } from 'react-dom/server';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { renderRoutes } from 'react-router-config'
+import { StaticRouter } from 'react-router-dom'
+import serverRoutes from '../frontend/routes/serverRouter'
+import reducer from '../frontend/reducers';
+import initialState from '../frontend/initialState';
 
 dotnev.config();
 
@@ -19,9 +28,8 @@ if (ENV === 'development') {
   app.use(webpackHotMiddleware(compiler))
 }
 
-app.get('*', (req, res) => {
-  console.log('hola')
-  res.send(`<!DOCTYPE html><html>
+const setResponse = (html) => {
+  return (`<!DOCTYPE html><html>
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -29,11 +37,25 @@ app.get('*', (req, res) => {
     <link rel="stylesheet" href="assets/app.css" type="text/css">
   </head>
   <body>
-      <div id="app"></div>
+      <div id="app">${html}</div>
       <script src="assets/app.js" type="text/javascript"></script>
   </body>
   </html>`)
-});
+}
+
+const renderApp = (req, res) => {
+  const store = createStore(reducer, initialState);
+  const html = renderToString(
+    <Provider store={store}>
+      <StaticRouter location={req.url} context={{}}>
+        {renderRoutes(serverRoutes)}
+      </StaticRouter>
+    </Provider>
+  )
+  res.send(setResponse(html))
+}
+
+app.get('*', renderApp);
 
 app.listen(PORT, (err) => {
   if (err) console.log(err)
