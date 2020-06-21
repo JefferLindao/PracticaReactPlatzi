ef901014a14320f8643d74ce6fff3aabc4a4ab69
JefferLindao <jefferlindao@gmail.com>
2020-06-21 10:54:52 -0500

    Optimizando el Build

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const webpack = require('webpack');
+const CompressionWebpackPlugin = require('compression-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+
 require('dotenv').config();
 
 const isDev = (process.env.ENV === 'development')
@@ -21,6 +24,10 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.jsx']
   },
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin()]
+  },
   module: {
     rules: [{
       test: /\.(js|jsx)$/,
@@ -28,12 +35,6 @@ module.exports = {
       use: {
         loader: 'babel-loader'
       }
-    },
-    {
-      test: /\.html$/,
-      use: [{
-        loader: 'html-loader'
-      }]
     },
     {
       test: /\.(s*)css$/,
@@ -62,6 +63,11 @@ module.exports = {
   plugins: [
     isDev ? new webpack.HotModuleReplacementPlugin() :
       () => { },
+    isDev ? () => { } :
+      new CompressionWebpackPlugin({
+        test: /\.js$|\.css$/,
+        filename: '[path].gz'
+      }),
     new MiniCssExtractPlugin({
       filename: 'assets/app.css'
     }),
